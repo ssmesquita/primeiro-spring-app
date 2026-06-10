2bfbc01249e6a06a5c90f94356b19261b0d45bdf
Sandro Mesquita <sandromesquita@yahoo.com>
2026-06-10 14:19:30 -0300

    COMMIT inicial

---
+++ src/main/java/com/example/primeiro_spring_app/PrimeiroSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.primeiro_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/primeiro_spring_app/PrimeiroSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeiro_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
